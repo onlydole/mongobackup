4d627ab092c57f58a1b80873c821fed5e1023614
Taylor.J.Dolezal <onlydole@gmail.com>
2018-01-23 22:43:35 -0800

    Add authentication flags

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 DATE=$(date +%Y%m%d_%H%M)
 echo "Starting backup of ${MONGO_SERVER} to s3://${S3_BUCKET}/${SUFFIX}/${DATE}"
-mongodump --host "${MONGO_SERVER}"  --gzip --out=/var/backups/${DATE}
+mongodump --host "${MONGO_SERVER}"  --username "${MONGO_USER}" --password "${MONGO_PASS}" --gzip --out=/var/backups/${DATE}
 
 aws s3 cp --recursive /var/backups/${DATE} s3://${S3_BUCKET}/${SUFFIX}/${DATE}
 
